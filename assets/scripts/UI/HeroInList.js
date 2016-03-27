7a21d6acc2f0c0ccf22e35db14fde06bec9eac17
Nan Wang <nantas@gmail.com>
2016-03-27 15:44:28 +0800

    complete layout of hero list panel

---
+++ assets/scripts/UI/HeroInList.js
@@ -0,0 +1,18 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        anim: cc.Animation,
+        sprite: cc.Sprite
+    },
+
+    // use this for initialization
+    init (heroInfo) {//{id, sf}
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
