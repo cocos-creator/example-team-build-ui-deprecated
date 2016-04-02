891a1240e88538cc676323c6706bc51773d06717
Nan Wang <nantas@gmail.com>
2016-04-02 17:10:04 +0800

    add scroll to hero function

--- assets/scripts/UI/TeamBuild.js
+++ assets/scripts/UI/TeamBuild.js
@@ -28,6 +28,7 @@ cc.Class({
         this.heroInfos = []; //{id, name, class, sf, iconAnchor, hp, atk, ap}
         this.lastContentPosX = 0;
         this.heroesInList = [];
+        this.finishedLayout = false;
         DataMng.loadHeroes(function(heroInfos) {
             this.heroInfos = heroInfos;
             this.onHeroLoaded();
@@ -71,6 +72,13 @@ cc.Class({
         this.scrollView._startAutoScroll(cc.pNeg(cc.p(this.scrollView.content.x - destPosX, 0)), this.snapTime, true);
     },
 
+    scrollToHeroIdx (idx) {
+        let destPosX = 0;
+        let length = this.heroesInList.length;
+        let contentPosX = -this.heroesInList[idx].x;
+        this.scrollToNext(contentPosX);
+    },
+
     onHeroLoaded () {
         let totalDist = 0;
         for (let i = 0; i < this.heroInfos.length; ++i ) {
@@ -89,8 +97,14 @@ cc.Class({
         this.labelAp.string = heroInfo.ap;
     },
 
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
-
-    // },
+    // this hack is to get when layout finished
+    lateUpdate: function (dt) {
+        if (this.finishedLayout) {
+            return;
+        }
+        if (this.listLayout._layoutDirty === false) {
+            this.scrollToHeroIdx(4);
+            this.finishedLayout = true;
+        }
+    },
 });
