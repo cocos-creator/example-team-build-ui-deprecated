04281522a7de7d3a68d86ba57711eed1a5df4903
Nan Wang <nantas@gmail.com>
2016-05-12 15:01:58 +0800

    update to v1.1

--- assets/scripts/Data/DataMng.js
+++ assets/scripts/Data/DataMng.js
@@ -16,8 +16,8 @@ module.exports = {
                     let anchorArr = heroInfo.portraitAnchor.split('|');
                     heroInfo.iconPos = cc.p(parseFloat(posArr[0]), parseFloat(posArr[1]));
                     heroInfo.portraitAnchor = cc.p(parseFloat(anchorArr[0]), parseFloat(anchorArr[1]));
-                    let sfUrl = 'heroes/' + heroInfo.id + '.png/' + heroInfo.id;
-                    cc.loader.loadRes(sfUrl, function(err, spriteFrame) {
+                    let sfUrl = 'heroes/' + heroInfo.id;
+                    cc.loader.loadRes(sfUrl, cc.SpriteFrame, function(err, spriteFrame) {
                         if (err) {
                             cc.log(err);
                         } else {
@@ -38,20 +38,29 @@ module.exports = {
             } else {
                 let list = data;
                 let count = list.length;
-                for (let i = 0; i < list.length; ++i) {
-                    let skillInfo = list[i];
-                    activeSkills[skillInfo.id] = skillInfo;
-                    cc.loader.loadRes('skills/' + skillInfo.icon, function(err, spriteFrame) {
-                        if (err) {
-                            cc.log(err);
-                        } else {
-                            skillInfo.sf = spriteFrame;
-                            if (--count <= 0) {
-                                return cb(list);
-                            }
-                        }
-                    });
-                }
+                cc.loader.loadRes("skills/skill_icons", cc.SpriteAtlas, function (err, atlas) {
+                    for (let i = 0; i < list.length; ++i) {
+                        let skillInfo = list[i];
+                        activeSkills[skillInfo.id] = skillInfo;
+                        let spriteFrame = atlas.getSpriteFrame(skillInfo.icon);
+                        skillInfo.sf = spriteFrame;
+                    }
+                    return cb(list);
+                });
+                // for (let i = 0; i < list.length; ++i) {
+                //     let skillInfo = list[i];
+                //     activeSkills[skillInfo.id] = skillInfo;
+                //     cc.loader.loadRes('skills/skill_icons' + skillInfo.icon, function(err, spriteFrame) {
+                //         if (err) {
+                //             cc.log(err);
+                //         } else {
+                //             skillInfo.sf = spriteFrame;
+                //             if (--count <= 0) {
+                //                 return cb(list);
+                //             }
+                //         }
+                //     });
+                // }
             }
         });
     },
@@ -62,20 +71,29 @@ module.exports = {
             } else {
                 let list = data;
                 let count = list.length;
-                for (let i = 0; i < list.length; ++i) {
-                    let skillInfo = list[i];
-                    passiveSkills[skillInfo.id] = skillInfo;
-                    cc.loader.loadRes('skills/' + skillInfo.icon, function(err, spriteFrame) {
-                        if (err) {
-                            cc.log(err);
-                        } else {
-                            skillInfo.sf = spriteFrame;
-                            if (--count <= 0) {
-                                return cb(list);
-                            }
-                        }
-                    });
-                }
+                cc.loader.loadRes("skills/skill_icons", cc.SpriteAtlas, function (err, atlas) {
+                    for (let i = 0; i < list.length; ++i) {
+                        let skillInfo = list[i];
+                        passiveSkills[skillInfo.id] = skillInfo;
+                        let spriteFrame = atlas.getSpriteFrame(skillInfo.icon);
+                        skillInfo.sf = spriteFrame;
+                    }
+                    return cb(list);
+                });
+                // for (let i = 0; i < list.length; ++i) {
+                //     let skillInfo = list[i];
+                //     passiveSkills[skillInfo.id] = skillInfo;
+                //     cc.loader.loadRes('skills/' + skillInfo.icon, function(err, spriteFrame) {
+                //         if (err) {
+                //             cc.log(err);
+                //         } else {
+                //             skillInfo.sf = spriteFrame;
+                //             if (--count <= 0) {
+                //                 return cb(list);
+                //             }
+                //         }
+                //     });
+                // }
             }
         });
     },
